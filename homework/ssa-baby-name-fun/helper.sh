ba29911d3a4116b262a8a9ffa1c480ec225d258e
Yuqing Pan <yuqingp@corn24.stanford.edu>
2015-01-23 21:59:08 -0800

    helper.sh

--- homework/ssa-baby-name-fun/helper.sh
+++ homework/ssa-baby-name-fun/helper.sh
@@ -1,7 +1,9 @@
 mkdir -p data-hold/names-by-state
+cd data-hold/names-by-state
 curl -o namesbystate.zip http://stash.compciv.org/ssa_baby_names/namesbystate.zip
 unzip namesbystate.zip
 cd ..
 mkdir -p names-nationwide
+cd names-nationwide
 curl -o names.zip http://stash.compciv.org/ssa_baby_names/names.zip
 unzip names.zip
