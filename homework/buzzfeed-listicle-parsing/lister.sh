d8a372aea78d9ce96b93ceeb77299d97fb4e9bf9
Yuqing Pan <yuqingp@corn06.stanford.edu>
2015-01-27 23:18:49 -0800

    buzzfeed

--- homework/buzzfeed-listicle-parsing/lister.sh
+++ homework/buzzfeed-listicle-parsing/lister.sh
@@ -2,7 +2,11 @@ d_start='2014-01-01'
 d_end='2014-12-31'
 days_diff=$(( ( $(date -ud $d_end +'%s') - $(date -ud $d_start +'%s') )/ 60 / 60 / 24 ))
 
+rm -f temp.txt
+
 for num in $(seq 0 $days_diff); do
-cat data-hold/2014/01/01.html | pup 'li.bf_dom a attr{href}' | cut -d/  -f3 | grep -oE '[[:digit:]]+-' | cut -d- -f1 | sort -n | uniq -c  
-date -d "$d_start $num days" +%Y-%m-%d
+  date_path=$(date -d "$d_start $num days" +%Y/%m/%d)
+  cat data-hold/${date_path}.html | pup 'li.bf_dom a attr{href}' | cut -d/  -f3 | grep -oE '\b[[:digit:]]{1,3}-' | cut -d- -f1 >> temp.txt
 done
+
+cat temp.txt | sort -n | uniq -c | sort -r
